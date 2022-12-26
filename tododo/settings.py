c59457e72720ac9babdf9db9703c732da90572d6
Aimedin <eliothab58@gmail.com>
2022-12-26 18:12:16 +0600

    delete cors

--- tododo/settings.py
+++ tododo/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders',
     'api'
 ]
 
@@ -42,7 +41,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -211,7 +209,3 @@ CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-
-CORS_ALLOWED_ORIGINS = [
-    'http://0.0.0.0:8000',
-]
